db407fe9cf7b3f88805b302b55be2c2e098a8399
rishabh dev <rishabhd921@gmail.com>
2023-01-19 10:26:21 +0530

    some changes in product page

--- expert-iron-7655/src/App.js
+++ expert-iron-7655/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import AllRoutes from './Components/AllRoutes'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+// import "./styles.css";
+import "swiper/css/bundle";
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
       <AllRoutes/>
     </div>
   )
